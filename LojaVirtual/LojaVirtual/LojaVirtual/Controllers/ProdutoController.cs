57c70aab0c1bc7f4fea6632a1b3007f91d5e64a9
andrey067 <audrey.ernesto.lima@gmail.com>
2020-09-28 18:50:39 -0400

    Montando as Paginas
    Criando as paginas de navegacao

---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        /*Todo controlador herda da classe Controller
+         * 
+         * E sempre vai retornar ActionResult ou IActionResult
+         * 
+         */
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+            return View(produto);
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One S",
+                Descricao = "4k Run",
+                Valor = 2000
+            };
+        }
+    }
+}
