66a60b2e53930cdc64d81b43d2567613fe9005ab
andrey067 <audrey.ernesto.lima@gmail.com>
2020-09-29 11:17:41 -0400

    Inicio requisicao email

--- LojaVirtual/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LojaVirtual.Controllers
@@ -18,16 +19,18 @@ namespace LojaVirtual.Controllers
             return View();
         }
 
-        public IActionResult ContatoAcao() 
+        public IActionResult ContatoAcao()
         {
-           string nome = HttpContext.Request.Form["nome"];
-            string email = HttpContext.Request.Form["email"];
-            string texto = HttpContext.Request.Form["texto"];
 
-            return new ContentResult() { Content = string.Format( "Dados recebidos com sucesso!! <br/>Nome: {0} <br/> E-email: {1} <br/>Texto {2}",nome,email,texto), ContentType = "text/html" };
+            Contato contato = new Contato();
+
+            contato.Nome = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto = HttpContext.Request.Form["texto"];
+            return new ContentResult() { Content = string.Format("Dados recebidos com sucesso!! <br/>Nome: {0} <br/> E-email: {1} <br/>Texto {2}", contato.Nome, contato.Email, contato.Texto), ContentType = "text/html" };
         }
 
-        public IActionResult Login() 
+        public IActionResult Login()
         {
             return View();
         }
---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(ContatoEmail contato)
+        {
+            /*
+             * Configurando requisicao SMTP do gmail
+             *  Servidor que vai enviar a mensagem
+             */
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("audrey.ernesto.lima@gmail.com","andrey067");
+            smtp.EnableSsl = true;
+
+            /*
+             * MailMEssage -> construir a mesagem
+             */
+
+            MailMessage mensagem = new MailMessage();
+
+        }
+    }
+}
